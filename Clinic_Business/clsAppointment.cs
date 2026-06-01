42740d5fd58395706721a8c9f27315b8710a4f60
Albara Fahed <alharissyalbara@example.com>
2026-06-02 01:18:12 +0300

    Add Class Appointment DL And BL

---
+++ Clinic_Business/clsAppointment.cs
@@ -0,0 +1,113 @@
+﻿using Clinic_DataAccess;
+using System;
+using System.Data;
+
+namespace Clinic_Business
+{
+    public class clsAppointment
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int AppointmentID { get; set; }
+        public int PatientID { get; set; }
+        public int DoctorID { get; set; }
+        public int CreatedByUserID { get; set; }
+        public short AppointmentStatus { get; set; }
+        public int AppointmentTypeID { get; set; }
+        public decimal AppointmentFees { get; set; }
+        public DateTime AppointmentDate { get; set; }
+
+        public clsAppointment()
+        {
+            this.AppointmentID = -1;
+            this.PatientID = -1;
+            this.DoctorID = -1;
+            this.CreatedByUserID = -1;
+            this.AppointmentStatus = 1; // Default: Scheduled
+            this.AppointmentTypeID = -1;
+            this.AppointmentFees = 0;
+            this.AppointmentDate = DateTime.Now;
+            Mode = enMode.AddNew;
+        }
+
+        private clsAppointment(int AppointmentID, int PatientID, int DoctorID, int CreatedByUserID,
+            short AppointmentStatus, int AppointmentTypeID, decimal AppointmentFees, DateTime AppointmentDate)
+        {
+            this.AppointmentID = AppointmentID;
+            this.PatientID = PatientID;
+            this.DoctorID = DoctorID;
+            this.CreatedByUserID = CreatedByUserID;
+            this.AppointmentStatus = AppointmentStatus;
+            this.AppointmentTypeID = AppointmentTypeID;
+            this.AppointmentFees = AppointmentFees;
+            this.AppointmentDate = AppointmentDate;
+            Mode = enMode.Update;
+        }
+
+        public static clsAppointment Find(int AppointmentID)
+        {
+            int PatientID = -1, DoctorID = -1, CreatedByUserID = -1, AppointmentTypeID = -1;
+            short AppointmentStatus = 1;
+            decimal AppointmentFees = 0;
+            DateTime AppointmentDate = DateTime.Now;
+
+            if (clsAppointmentData.GetAppointmentByID(AppointmentID, ref PatientID, ref DoctorID, ref CreatedByUserID,
+                ref AppointmentStatus, ref AppointmentTypeID, ref AppointmentFees, ref AppointmentDate))
+            {
+                return new clsAppointment(AppointmentID, PatientID, DoctorID, CreatedByUserID,
+                    AppointmentStatus, AppointmentTypeID, AppointmentFees, AppointmentDate);
+            }
+            return null;
+        }
+
+        private bool _AddNewAppointment()
+        {
+            this.AppointmentID = clsAppointmentData.AddNewAppointment(this.PatientID, this.DoctorID, this.CreatedByUserID,
+                this.AppointmentStatus, this.AppointmentTypeID, this.AppointmentFees, this.AppointmentDate);
+            return (this.AppointmentID != -1);
+        }
+
+        private bool _UpdateAppointment()
+        {
+            return clsAppointmentData.UpdateAppointment(this.AppointmentID, this.PatientID, this.DoctorID, this.CreatedByUserID,
+                this.AppointmentStatus, this.AppointmentTypeID, this.AppointmentFees, this.AppointmentDate);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewAppointment())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    return false;
+                case enMode.Update:
+                    return _UpdateAppointment();
+            }
+            return false;
+        }
+
+        public static bool DeleteAppointment(int AppointmentID)
+        {
+            return clsAppointmentData.DeleteAppointment(AppointmentID);
+        }
+
+        public static DataTable GetAllAppointments()
+        {
+            return clsAppointmentData.GetAllAppointments();
+        }
+
+        public static DataRow GetAppointmentInfo(int AppointmentID)
+        {
+            return clsAppointmentData.GetAppointmentInfoByID(AppointmentID);
+        }
+        public static bool IsExists(int AppointmentID)
+        {
+            return clsAppointmentData.DoesAppointmentExist(AppointmentID);
+        }
+    }
+}
---
+++ Clinic_DataAccess/clsAppointmentData.cs
@@ -0,0 +1,268 @@
+﻿using Clinic_DataAccess.SaveSqlException;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Clinic_DataAccess
+{
+    public class clsAppointmentData
+    {
+        public static bool GetAppointmentByID(int AppointmentID, ref int PatientID, ref int DoctorID,
+            ref int CreatedByUserID, ref short AppointmentStatus, ref int AppointmentTypeID,
+            ref decimal AppointmentFees, ref DateTime AppointmentDate)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = @"SELECT * FROM Appointments WHERE AppointmentID = @AppointmentID";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AppointmentID", AppointmentID);
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+
+                                PatientID = (int)reader["PatientID"];
+                                DoctorID = (int)reader["DoctorID"];
+                                CreatedByUserID = (int)reader["CreatedByUserID"];
+                                AppointmentStatus = (short)reader["AppointmentStatus"];
+                                AppointmentTypeID = (int)reader["AppointmentTypeID"];
+                                AppointmentFees = (decimal)reader["AppointmentFees"];
+                                AppointmentDate = (DateTime)reader["AppointmentDate"];
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                clsGlobalLogger.LogSqlException(ex, clsGlobalLogger.LogLevel.Error);
+                return false;
+            }
+            return false;
+        }
+
+
+        public static DataRow GetAppointmentInfoByID(int AppointmentID)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "SELECT * FROM View_AppointmentsDetails WHERE AppointmentID = @AppointmentID;";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AppointmentID", AppointmentID);
+
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows) dt.Load(reader);
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                clsGlobalLogger.LogSqlException(ex, clsGlobalLogger.LogLevel.Error);
+            }
+
+            return (dt.Rows.Count > 0) ? dt.Rows[0] : null;
+        }
+
+        public static int AddNewAppointment(int PatientID, int DoctorID, int CreatedByUserID,
+            short AppointmentStatus, int AppointmentTypeID, decimal AppointmentFees, DateTime AppointmentDate)
+        {
+            int AppointmentID = -1;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = @"INSERT INTO Appointments (PatientID, DoctorID, CreatedByUserID, AppointmentStatus, AppointmentTypeID, AppointmentFees, AppointmentDate)
+                                 VALUES (@PatientID, @DoctorID, @CreatedByUserID, @AppointmentStatus, @AppointmentTypeID, @AppointmentFees, @AppointmentDate);
+                                 SELECT SCOPE_IDENTITY();";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@PatientID", PatientID);
+                        command.Parameters.AddWithValue("@DoctorID", DoctorID);
+                        command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+                        command.Parameters.AddWithValue("@AppointmentStatus", AppointmentStatus);
+                        command.Parameters.AddWithValue("@AppointmentTypeID", AppointmentTypeID);
+                        command.Parameters.AddWithValue("@AppointmentFees", AppointmentFees);
+                        command.Parameters.AddWithValue("@AppointmentDate", AppointmentDate);
+
+
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                            AppointmentID = insertedID;
+                    }
+                }
+
+            }
+
+            catch (SqlException ex)
+            {
+                clsGlobalLogger.LogSqlException(ex, clsGlobalLogger.LogLevel.Error, CreatedByUserID);
+            }
+
+            return AppointmentID;
+        }
+
+        public static bool UpdateAppointment(int AppointmentID, int PatientID, int DoctorID, int CreatedByUserID,
+            short AppointmentStatus, int AppointmentTypeID, decimal AppointmentFees, DateTime AppointmentDate)
+        {
+            int rowsAffected = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = @"UPDATE Appointments SET PatientID = @PatientID, DoctorID = @DoctorID, 
+                                 CreatedByUserID = @CreatedByUserID, AppointmentStatus = @AppointmentStatus, 
+                                 AppointmentTypeID = @AppointmentTypeID, AppointmentFees = @AppointmentFees, 
+                                 AppointmentDate = @AppointmentDate
+                                 WHERE AppointmentID = @AppointmentID";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AppointmentID", AppointmentID);
+                        command.Parameters.AddWithValue("@PatientID", PatientID);
+                        command.Parameters.AddWithValue("@DoctorID", DoctorID);
+                        command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+                        command.Parameters.AddWithValue("@AppointmentStatus", AppointmentStatus);
+                        command.Parameters.AddWithValue("@AppointmentTypeID", AppointmentTypeID);
+                        command.Parameters.AddWithValue("@AppointmentFees", AppointmentFees);
+                        command.Parameters.AddWithValue("@AppointmentDate", AppointmentDate);
+
+
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                clsGlobalLogger.LogSqlException(ex, clsGlobalLogger.LogLevel.Error, CreatedByUserID);
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        public static bool DeleteAppointment(int AppointmentID)
+        {
+            int rowsAffected = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "DELETE FROM Appointments WHERE AppointmentID = @AppointmentID";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AppointmentID", AppointmentID);
+
+
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                clsGlobalLogger.LogSqlException(ex, clsGlobalLogger.LogLevel.Error);
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        public static DataTable GetAllAppointments()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "SELECT * FROM View_AppointmentsDetails";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.HasRows) dt.Load(reader);
+                        }
+                    }
+                }
+            }
+
+            catch (SqlException ex)
+            {
+                clsGlobalLogger.LogSqlException(ex, clsGlobalLogger.LogLevel.Error);
+            }
+
+            return dt;
+        }
+
+
+        public static bool DoesAppointmentExist(int AppointmentID)
+        {
+            bool IsFound = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "SELECT Found = 1 FROM Appointments WHERE AppointmentID = @AppointmentID";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AppointmentID", AppointmentID);
+
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null) IsFound = true;
+
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                clsGlobalLogger.LogSqlException(ex, clsGlobalLogger.LogLevel.Error);
+                IsFound = false;
+            }
+
+            return IsFound;
+        }
+
+        public static bool UpdateAppointmentStatus(int AppointmentID, short NewStatus)
+        {
+            int rowsAffected = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    string query = "UPDATE Appointments SET AppointmentStatus = @NewStatus WHERE AppointmentID = @AppointmentID";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AppointmentID", AppointmentID);
+                        command.Parameters.AddWithValue("@NewStatus", NewStatus);
+
+                        connection.Open();
+                        rowsAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                clsGlobalLogger.LogSqlException(ex, clsGlobalLogger.LogLevel.Error);
+            }
+
+            return (rowsAffected > 0);
+        }
+    }
+}
